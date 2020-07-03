9fbdb075b668404f3cc3ead1a5b8c319d1e9aff9
the_derek <thederekfawcett@gmail.com>
2020-07-03 06:38:17 -0400

    can now simulate battles (basically)
    Signed-off-by: the_derek <thederekfawcett@gmail.com>

---
+++ src/pokemans/engine/WeatherMultipliers.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020, Derek Fawcett. All rights reserved. No usage without permission. */
+
+// **This file is auto-generated**
+
+package pokemans.engine;
+
+import java.math.BigDecimal;
+
+interface WeatherMultipliers {
+    
+    BigDecimal weatherattackBonusMultiplier = new BigDecimal("1.2");
+    BigDecimal cpBaseLevelBonus = new BigDecimal("5.0");
+    BigDecimal guaranteedIndividualValues = new BigDecimal("4.0");
+    BigDecimal raidEncounterCpBaseLevelBonus = new BigDecimal("5.0");
+    BigDecimal raidEncounterGuaranteedIndividualValues = new BigDecimal("10.0");
+    BigDecimal stardustBonusMultiplier = new BigDecimal("1.25");
+    
+}
